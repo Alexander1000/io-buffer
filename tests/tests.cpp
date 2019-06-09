4ebd942e94e3f35b786775c3605a31e1d21125da
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-06-09 13:59:36 +0300

    add test case

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,3 +1,33 @@
+#include <cpp-unit-test.h>
+#include <io-buffer.h>
+#include <iostream>
+#include <string>
+
+CppUnitTest::TestCase* testCase_SmallFile_Positive()
+{
+    CppUnitTest::TestCase* t = new CppUnitTest::TestCase("small-file");
+    t->printTitle();
+
+    IOBuffer::IOFileReader file_buffer("../fixtures/001-test-case-small-file.txt");
+    IOBuffer::CharStream charStream(&file_buffer);
+
+    char* curChar = NULL;
+
+    std::string assertCharacters = "It is some file for test...\n";
+
+    for (int i = 0; i < assertCharacters.length(); i++) {
+        curChar = charStream.getNext();
+        CppUnitTest::assertNotNull(t, curChar);
+        CppUnitTest::assertEquals(t, assertCharacters[i], *curChar);
+    }
+
+    t->finish();
+    return t;
+}
+
 int main(int argc, char** argv) {
+    CppUnitTest::TestSuite testSuite;
+    testSuite.addTestCase(testCase_SmallFile_Positive());
+    testSuite.printTotal();
     return 0;
 }
